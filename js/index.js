720ff2767c8e001b5ababbfe74ecf8e881568d4e
Aukjevd <a.van.doremalen@sintlucasedu.nl>
2023-03-22 16:38:08 +0100

    4

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 document.getElementById("mainTitle").innerText = "Point and click adventure";
+
+const gameWindow = document.getElementById("gameWindow");
+const mainCharacter = document.getElementById("mainCharacter");
+const offsetCharacter = 16;
+
+gameWindow.onclick = function(e){
+    var rect = gameWindow.getBoundingClientRect();
+    var x = e.clientX - rect.left;
+    var y = e.clientY - rect.top;
+ 
+    mainCharacter.style.left = x - offsetCharacter +"px";
+    mainCharacter.style.top = y - offsetCharacter +"px";
+    
+    console.log(e.target.id)
+
+    switch(e.target.id){
+        case "door":
+            console.log("this door is closed")
+            break
+        case "chest":
+            console.log("this chest is empty")
+            break
+        default:
+            console.log("I don't know what I want to do")
+            break
+    }
+    
+}
